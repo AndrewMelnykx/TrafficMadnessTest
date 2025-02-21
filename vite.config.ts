28ba34b24291b94c9209c2cd9f75e83e867dadf4
Andrew Melnykx <andreymelnyk777@gmail.com>
2025-02-21 03:38:25 +0100

    Fourth attempt

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Traffic_Madness_Test/",
+  base: "/TrafficMadnessTest/",
   build: {
     outDir: path.resolve(__dirname, "dist"),
   },
